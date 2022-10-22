af428d14c3aaa696b205a2765b797fbebd041132
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-22 14:12:25 +0000

    setCart - using copying previous cart, then add new product.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -9,7 +9,7 @@ const Shop = () => {
   console.log(cart);
 
   const handleAddToCart = (product) => {
-    setCart(product);
+    setCart((previous) => [...previous, product]);
   };
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
