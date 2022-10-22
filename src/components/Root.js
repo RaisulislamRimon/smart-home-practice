4e1d43056219d699f67dc626dc320b0de55754f1
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-22 12:06:59 +0000

    now shop component render products by using ProductContext, not using props drilling...

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,12 +2,14 @@ import React, { createContext } from "react";
 import { Outlet, useLoaderData } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import Product from "./Product";
 
 export const ProductContext = createContext();
 
 const Root = () => {
+  const products = useLoaderData();
   return (
-    <ProductContext.Provider value={[]}>
+    <ProductContext.Provider value={products}>
       <Header />
       <Outlet />
       <Footer />
