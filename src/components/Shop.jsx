701e265c9b75c3531eeb299d0554cbe2ef160a82
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-22 14:08:41 +0000

    error while adding cart

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Product from "./Product";
 import { ProductContext } from "./Root";
 
 const Shop = () => {
   const products = useContext(ProductContext);
+
+  const [cart, setCart] = useState([]);
+  console.log(cart);
+
   const handleAddToCart = (product) => {
-    console.log("clicked", product);
+    setCart(product);
   };
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
