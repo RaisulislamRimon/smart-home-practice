f3e3feabd20a7116a05ac3ba799f8cdc02860306
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-22 11:43:06 +0000

    creating ProductContext

--- src/components/Root.js
+++ src/components/Root.js
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 
+export const ProductContext = createContext();
+
 const Root = () => {
   return (
     <div>
