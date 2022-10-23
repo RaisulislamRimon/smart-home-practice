d6e8478cc775ea2198d2f0cc74a3c162067fb5f5
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-23 10:06:48 +0000

    add to cart and save it into localStorage.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { toast } from "react-toastify";
 import { addToDb } from "../utils/fakeDB";
 import Product from "./Product";
 import { ProductContext } from "./Root";
@@ -26,6 +27,7 @@ const Shop = () => {
     }
     setCart(newCart);
     addToDb(product.id);
+    toast.success("Product added!", { autoClose: 5000 });
   };
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
